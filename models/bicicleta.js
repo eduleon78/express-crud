f0b8295751784e182c4a0d09d31fe7e4a99efb23
eduleon78 <66341077+eduleon78@users.noreply.github.com>
2021-08-02 14:19:02 -0400

    get post delete en api, probado con postman

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -31,8 +31,8 @@ Bicicleta.removeById = function(aBiciId){
     }
 }
 
-var a = new Bicicleta(1, 'rojo', 'urbana', [10.980197, -63.846129]);
-var b = new Bicicleta(1, 'rojo', 'urbana', [10.982609, -63.844369]);
+var a = new Bicicleta(1, 'fucsia', 'urbana', [10.980197,-63.846129]);
+var b = new Bicicleta(2, 'naranja', 'urbana', [10.982609,-63.844369]);
 
 Bicicleta.add(a);
 Bicicleta.add(b);
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,9 +4,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-
-var marker = L.marker([10.969136, -63.858322]).addTo(map);
-var marker = L.marker([10.975314, -63.849984]).addTo(map);
-var marker = L.marker([10.973468, -63.852968]).addTo(map);
-
-
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
